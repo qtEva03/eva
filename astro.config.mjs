834e520b13312377a4675ab81a332add90de5ad3
Eva <eva.evangelisti03@gmail.com>
2024-05-05 23:54:22 +0200

    Update 404 page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: "https://www.evaevangelisti.com"
+    site: "https://www.evaevangelisti.com",
+    redirects: {
+        "/index.html": "/"
+    }
 });
