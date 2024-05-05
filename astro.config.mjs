316739ae066073036b2b1ce64c9adda0992ef42b
Eva <eva.evangelisti03@gmail.com>
2024-05-06 00:08:09 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-    site: "https://www.evaevangelisti.com"
+    site: "https://www.evaevangelisti.com",
+    integrations: [sitemap()],
 });
